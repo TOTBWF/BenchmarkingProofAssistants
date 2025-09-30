f36f67086486c4b775852fc4ebf1cd056fb1ae6c
Reed Mullanix <reedmullanix@gmail.com>
2025-09-30 16:45:46 -0400

    test: snapshot lean inductive tests

--- panbench/generators/test/snapshot/IndicesParameters_Datatypes.lean
+++ panbench/generators/test/snapshot/IndicesParameters_Datatypes.lean
@@ -1,3 +1,3 @@
 
-inductive D (p1 : Type) (p2 : Type) (p3 : Type) (p4 : Type) (p5 : Type) : Nat -> Nat -> Nat -> Nat -> Nat -> Type where
-| C : {X1 X2 X3 X4 X5 : Nat} -> D p1 p2 p3 p4 p5 X1 X2 X3 X4 X5
+inductive D (p₁ : Type) (p₂ : Type) (p₃ : Type) (p₄ : Type) (p₅ : Type) : Nat -> Nat -> Nat -> Nat -> Nat -> Type where
+  | C : {x₁ x₂ x₃ x₄ x₅ : Nat} → D p₁ p₂ p₃ p₄ p₅ x₁ x₂ x₃ x₄ x₅
