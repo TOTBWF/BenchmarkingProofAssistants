5b1f8f33d727931b03088a004bdb8d217aae1586
Reed Mullanix <reedmullanix@gmail.com>
2025-09-30 11:17:03 -0400

    fix: adjustments to NestedLetFunction generator

--- panbench/generators/src/Panbench/Generator/NestedLetFunctions.hs
+++ panbench/generators/src/Panbench/Generator/NestedLetFunctions.hs
@@ -7,14 +7,14 @@ import Panbench
 
 generator :: _ => GenModule Natural hdr defns
 generator =
-    GenModule "LetAddExample"
+    GenModule "NestedFunction"
       [ import_ "Data.Nat"
       ] \size ->
       [ ([] |- ("n" .: builtin "Nat")) .=
           let_ [
-            [ nameN "x" j .: builtin "Nat" | j <- [1..i] ] |- syn (nameN "f" i) .=
+            [ nameN "x" j .: builtin "Nat" | j <- [1..i] ] |- (nameN "f" i .: builtin "Nat") .=
                 foldl (op2 "+") (nat 1) [ nameN "x" j | j <- [1..i]]
             | i <- [1..size]
             ] $
-          foldr (\i -> op2 "+" (app (nameN "f" i) [ nat j | j <- [1..i] ])) (app (nameN "f" 1) [nat 2]) [2..size]
+          foldl (\tm i -> op2 "+" (app (nameN "f" i) [ nat j | j <- [2..i+1] ]) tm) (app (nameN "f" 1) [nat 2]) [2..size]
       ]
