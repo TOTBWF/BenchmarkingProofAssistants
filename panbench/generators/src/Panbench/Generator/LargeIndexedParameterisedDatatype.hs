8804d0dfa4db55771f67918254e184870f0814c0
Reed Mullanix <reedmullanix@gmail.com>
2025-09-30 15:13:11 -0400

    fix: mark implicits

--- panbench/generators/src/Panbench/Generator/LargeIndexedParameterisedDatatype.hs
+++ panbench/generators/src/Panbench/Generator/LargeIndexedParameterisedDatatype.hs
@@ -19,6 +19,6 @@ generator =
   [ import_ "Data.Nat"
   ] \size ->
   [ data_ ([ nameN "p" i .: builtin "Type" | i <- [1..size]] |- "D" .: foldr (\_ tp -> anonChk (builtin "Nat") `arr` tp) (builtin "Type") [1..size])
-    [ "C" .: pi [[ nameN "x" i | i <- [1..size] ] .:* builtin "Nat"] (app "D" ([ nameN "p" i | i <- [1..size] ] ++ [ nameN "x" i | i <- [1..size] ]))
+    [ "C" .: pi [implicit ([ nameN "x" i | i <- [1..size] ] .:* builtin "Nat")] (app "D" ([ nameN "p" i | i <- [1..size] ] ++ [ nameN "x" i | i <- [1..size] ]))
     ]
   ]
