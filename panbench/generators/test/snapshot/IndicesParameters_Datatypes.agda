466e98c8db1ea73a44cd79f3c9ef35ee24a6c418
Reed Mullanix <reedmullanix@gmail.com>
2025-09-30 15:11:12 -0400

    fix: mark constructors as implicit in IndicesParameters

--- panbench/generators/test/snapshot/IndicesParameters_Datatypes.agda
+++ panbench/generators/test/snapshot/IndicesParameters_Datatypes.agda
@@ -3,4 +3,4 @@ module IndicesParameters_Datatypes where
 open import Agda.Builtin.Nat
 
 data D (p₁ : Set) (p₂ : Set) (p₃ : Set) (p₄ : Set) (p₅ : Set) : Nat → Nat → Nat → Nat → Nat → Set where
-  C : (x₁ x₂ x₃ x₄ x₅ : Nat) → D p₁ p₂ p₃ p₄ p₅ x₁ x₂ x₃ x₄ x₅
+  C : {x₁ x₂ x₃ x₄ x₅ : Nat} → D p₁ p₂ p₃ p₄ p₅ x₁ x₂ x₃ x₄ x₅
--- panbench/generators/test/snapshot/IndicesParameters_Datatypes.v
+++ panbench/generators/test/snapshot/IndicesParameters_Datatypes.v
@@ -1,7 +1,7 @@
 
-
 Module IndicesParameters_Datatypes.
 
 Inductive D (p1 : Type) (p2 : Type) (p3 : Type) (p4 : Type) (p5 : Type) : nat -> nat -> nat -> nat -> nat -> Type :=
-| C : forall {X1 X2 X3 X4 X5 : nat}, D p1 p2 p3 p4 p5 X1 X2 X3 X4 X5.
+| C : forall {x1 x2 x3 x4 x5 : nat}, D p1 p2 p3 p4 p5 x1 x2 x3 x4 x5.
+
 End IndicesParameters_Datatypes.
